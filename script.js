a69766a5c4fd3990ac9bca2d94ebe5684ae6a772
GanisherHam <ganisherkhamrayev@gmail.com>
2021-12-26 02:36:41 +0300

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll(`.slide`)
+
+slides.forEach(slide => {
+    slide.addEventListener(`click`, function() {
+        removeActive(slides);
+        this.classList.add(`active`);
+    })
+    console.log(slide);
+})
+
+// this  в обработчике события обращается к элементу на который вушаем обработчик события.
+
+function removeActive(array) {
+    array.forEach(item => {
+        item.classList.remove(`active`)
+    })
+}
+
+// function addActive() {
+
+// }
